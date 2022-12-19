74d751c946364b4675e5b6de3342f0da014108ac
Jiří Kvant Kubík <jiri.kub@gmail.com>
2022-12-19 13:04:06 +0100

    [python] adding gcode auto-generation.

--- temperature-tower-generator.py
+++ temperature-tower-generator.py
@@ -1,11 +1,18 @@
-import os, sys, subprocess
+import os
+
+PRUSA_PROFILE_PATH = ""
 
 def produce_stl(min_temp, max_temp, step_temp, fn="temp-tower-test.stl"):
-    tmp_fn = "_tmp_tn.txt"
     cmd_template = "openscad -o {} source/tower.scad -D min_temp={} -D max_temp={} -D step_temp={}"
-    cmd = cmd_template.format(fn, min_temp, max_temp, step_temp, tmp_fn)
+    cmd = cmd_template.format(fn, min_temp, max_temp, step_temp)
+    os.system(cmd)
+
+def produce_gcode(fn_in="temp-tower-test.stl", fn_out="temp-tower-test.gcode", fn_bundle="test-config.ini"):
+    cmd_template = "prusaslicer -g {} --load {} -o {}"
+    cmd = cmd_template.format(fn_in, fn_bundle, fn_out)
     os.system(cmd)
 
 if __name__=="__main__":
     produce_stl(200,210,5)
+    produce_gcode()
     # TODO ARG-parse
