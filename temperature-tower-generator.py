0b5099abcc2f5c0f9f9b4b680ee433baa33efcff
Jiří Kvant Kubík <jiri.kub@gmail.com>
2022-12-19 13:21:09 +0100

    [python] adding processing config bundle to extract layer and first layer heights.

--- temperature-tower-generator.py
+++ temperature-tower-generator.py
@@ -12,7 +12,25 @@ def produce_gcode(fn_in="temp-tower-test.stl", fn_out="temp-tower-test.gcode", f
     cmd = cmd_template.format(fn_in, fn_bundle, fn_out)
     os.system(cmd)
 
+def processs_gcode(temperatures, fn_in="temp-tower-test.gcode", fn_out="final.gcode", fn_bundle="test-config.ini"):
+    layer_height = 0
+    first_layer_height = 0
+
+    # get layer and first layer height
+    with open(fn_bundle, 'r') as f:
+        lines = f.readlines()
+        layer_height = float([line for line in lines if "layer_height" in line and not "_layer" in line and not "{" in line][0].strip().split(" ")[-1])
+        first_layer_height = float([line for line in lines if "first_layer_height" in line][0].strip().split(" ")[-1])
+
+    pass
+
 if __name__=="__main__":
-    produce_stl(200,210,5)
-    produce_gcode()
+    _min_temp = 200
+    _max_temp = 210
+    _step_temp = 5
+    #produce_stl(_min_temp,_max_temp,_step_temp)
+    #produce_gcode()
+    temperatures = [i for i in range(_min_temp,_max_temp+1,_step_temp)]
+    print(temperatures)
+    processs_gcode(temperatures=temperatures)
     # TODO ARG-parse
