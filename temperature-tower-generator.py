2915cd1f44c2063bf08a9ee382e0e8ce52e2d0d2
Jiří Kvant Kubík <jiri.kub@gmail.com>
2022-12-19 14:13:45 +0100

    [python] adding temperature change layer identification.

--- temperature-tower-generator.py
+++ temperature-tower-generator.py
@@ -18,11 +18,37 @@ def processs_gcode(temperatures, fn_in="temp-tower-test.gcode", fn_out="final.gc
 
     # get layer and first layer height
     with open(fn_bundle, 'r') as f:
-        lines = f.readlines()
-        layer_height = float([line for line in lines if "layer_height" in line and not "_layer" in line and not "{" in line][0].strip().split(" ")[-1])
-        first_layer_height = float([line for line in lines if "first_layer_height" in line][0].strip().split(" ")[-1])
+        bundle_lines = f.readlines()
+        layer_height = float([line for line in bundle_lines if "layer_height" in line and not "_layer" in line and not "{" in line][0].strip().split(" ")[-1])
+        first_layer_height = float([line for line in bundle_lines if "first_layer_height" in line][0].strip().split(" ")[-1])
 
-    pass
+    # TODO make this constant/autoloadable
+    base_height = 1
+    floor_height = 10
+    
+    # get all avaliable layers from the gcode
+    gcode_lines = ""
+    with open(fn_in, 'r') as f:
+        gcode_lines = "".join(f.readlines())
+    n_layers = gcode_lines.count("\n;AFTER_LAYER_CHANGE")
+    gcode_layers = [first_layer_height+i*layer_height for i in range(n_layers)]
+
+    # get the precise layers for temperature change
+    n_floors = len(temperatures)
+    floors_layers = [base_height+i*floor_height for i in range(n_floors)]
+    print(floors_layers)
+    temp_change_at = []
+    for fl in floors_layers:
+        diff = gcode_layers[-1]
+        idx = None
+        for l_idx in range(len(gcode_layers)):
+            l = gcode_layers[l_idx]
+            if abs(fl-l) < diff:
+                diff = abs(fl-l)
+                idx = l_idx
+                print(diff, idx)
+        temp_change_at.append(idx+1)
+    print(["{:.3f}".format(gcode_layers[i]) for i in temp_change_at])
 
 if __name__=="__main__":
     _min_temp = 200
